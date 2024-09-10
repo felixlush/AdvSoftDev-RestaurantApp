ab2393094f9fb151b707444f1013348a0afabf49
felixlush <92777513+felixlush@users.noreply.github.com>
2024-09-10 14:55:07 +1000

    Added view panel logic

--- app/ui/Account/Dashboard/OrderViewPanel.tsx
+++ app/ui/Account/Dashboard/OrderViewPanel.tsx
@@ -22,17 +22,22 @@ export default function OrderViewPanel(props: OrderViewProps) {
     }, [])
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex grid-cols-2 grid-rows-4 items-center justify-center z-50">
-            <div className="bg-white p-6 rounded-lg shadow-lg w-1/3">
-                <h1>Order: {props.order.order_id}</h1>
-                <div>
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg w-2/3 h-3/5 overflow-scroll">
+                <div className='flex justify-evenly'>
+                    <h1 className='font-bold text-lg'>Order ID: {props.order.order_id}</h1>
+                    <p className='text-gray-400'>Date: {props.order.order_status}</p>
+                </div>
+                <div className='gap-4'>
                     {orderItems.map(orderItem => (
-                        <div className='flex-col p-5 justify-evenly overflow-y-auto overflow-x-auto'>
-                            <p>Quantity: {orderItem.quantity}</p>
+                        <div className='flex p-5 justify-evenly overflow-y-auto border rounded-lg shadow hover:shadow-lg transition gap-4'>
                             {orderItem.image_url && (
-                                <img src={orderItem.image_url} alt={orderItem.item_name} className="rounded-md mt-2 max-h-10" />
+                                <img src={orderItem.image_url} alt={orderItem.item_name} className="rounded-md mt-2 max-h-20 mb-2" />
                             )}
-                            <p>{orderItem.item_name}</p>
+                            <div className='flex-col'>
+                                <p>{orderItem.item_name}</p>
+                                <p className='text-sm text-gray-500 mt-4'>Quantity: {orderItem.quantity}</p>
+                            </div>
                         </div>
                     ))}
                 </div>
