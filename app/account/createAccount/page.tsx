c2311044c9b97535eb2a2d9052f6344e5606478a
felixlush <92777513+felixlush@users.noreply.github.com>
2024-10-29 15:53:18 +1100

    Added pad to create account

--- app/account/createAccount/page.tsx
+++ app/account/createAccount/page.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 export default function CreateAccount() {
     return (
-        <>
+        <div className='pt-40'>
             <SignupForm />
-        </>
+        </div>
     )
 }
